a847aa27e07c1cb3b52ae80538f0b75affb0dbb9
Damian W. Betebenner <dbetebenner@nciea.org>
2026-02-06 06:40:02 +0000

    version 1.4-0.0

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,47 @@
 function(libname, pkgname) {
 }
 
-`.onAttach` <-
-function(libname, pkgname) {
-	if (interactive()) {
-		packageStartupMessage(magenta$bold('toOrdinal',paste(paste0(unlist(strsplit(as.character(packageVersion("toOrdinal")), "[.]")), c(".", "-", ".", "")), collapse=""),' (2-18-2022). For help: >help("toOrdinal") or visit https://centerforassessment.github.io/toOrdinal/'))
-	}
+`.onAttach` <- function(libname, pkgname) {
+    if (interactive()) {
+		# Utility function 
+        get_dev_version <- function(package) {
+            url <- paste0("https://raw.githubusercontent.com/CenterForAssessment/", package, "/refs/heads/master/DESCRIPTION")
+            tryCatch({
+                lines <- readLines(url, warn = FALSE)
+                version_line <- grep("^Version:", lines, value = TRUE)
+                if (length(version_line) > 0) {
+                    return(cyan("v", strsplit(version_line, ": ")[[1]][2], sep=""))
+                } else {
+                    return(red("Not Available"))
+                }
+            }, error = function(e) {
+                return(red("Not Available"))
+            }, warning = function(w) {
+                return(red("Not Available"))
+            })
+        }
+
+        # Extract version information
+        installed.version <- utils::packageDescription("toOrdinal")[['Version']]
+        cran.version <- tryCatch(
+            green("v", pkgsearch::cran_package("toOrdinal")[['Version']], sep=""),
+            error = function(e) red("Not Available"),
+            warning = function(w) red("Not Available"))
+        dev.version <- get_dev_version("toOrdinal")
+
+        # Define a friendly startup message
+		message_text <- paste0(
+		    magenta(bold("\uD83C\uDF89 toOrdinal v", installed.version, sep="")), " - ", toOrdinal::toOrdinalDate("2026-2-5"), "\n",
+			strrep("\u2501", 40), "\n",
+    	    bold("\U1F4E6 CRAN: "), cran.version, "\n",
+    	    bold("\U1F527 Dev: "), dev.version, "\n",
+			strrep("\u2501", 40), "\n",
+		    "\U1F4A1 Tip: ", magenta(bold("> help(package=\"toOrdinal\")")), "\n",
+		    "\U1F310 Docs: ", magenta(bold("https://centerforassessment.github.io/toOrdinal")), "\n",
+			strrep("\u2501", 40), "\n",
+		    "\u2728 Happy toOrdinaling!")
+
+        # Display the startup message
+        packageStartupMessage(message_text)
+    }
 }
